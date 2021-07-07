2a69d6bade2e838048c229c35b56ec9e19d94336
strantr <strantr>
2021-07-07 20:06:28 +0100

    Added tests

---
+++ tests/function.spec.ts
@@ -0,0 +1,19 @@
+describe("snoopy", () => {
+	describe("function:before", () => {
+		it("receives arguments", () => {});
+		it("receives 'this' context", () => {});
+		it("can replace individual arguments", () => {});
+		it("can replace all arguments", () => {});
+		it("can replace 'this' context", () => {});
+		it("can throw exception", () => {});
+		it("can return early", () => {});
+	});
+
+	describe("function:after", () => {
+		it("receives used arguments", () => {});
+		it("receives used 'this' context", () => {});
+		it("receives original arguments", () => {});
+		it("receives original 'this' context", () => {});
+		it("receives return value", () => {});
+	});
+});
