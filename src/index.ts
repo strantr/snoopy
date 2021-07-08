046d516dea5b9126fe6aaf694df22f5de64e7594
strantr <strantr>
2021-07-08 20:07:40 +0100

    Implemented function patch after tests Implemented function patching

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,11 @@ interface AfterCallbackContext<TFunction extends AnyFunction> extends PatchCallb
 	};
 	returnValue: ReturnType<TFunction>;
 }
-type PromisableCallback<TFunction extends AnyFunction> = ReturnType<TFunction> extends PromiseLike<any>
-	? PromiseLike<typeof snoopycall>
+type PromishCallback<TFunction extends AnyFunction> = ReturnType<TFunction> extends Promise<any>
+	? Promise<typeof snoopycall>
 	: typeof snoopycall;
 
-type ConfigCallback<TFunction extends AnyFunction, TContext> = (context: TContext) => PromisableCallback<TFunction>;
+type ConfigCallback<TFunction extends AnyFunction, TContext> = (context: TContext) => PromishCallback<TFunction>;
 type BeforeCallback<TFunction extends AnyFunction> = ConfigCallback<TFunction, BeforeCallbackContext<TFunction>>;
 type AfterCallback<TFunction extends AnyFunction> = ConfigCallback<TFunction, AfterCallbackContext<TFunction>>;
 
@@ -31,8 +31,151 @@ interface PatchConfig<T extends AnyFunction> {
 }
 
 class Snoopy {
+	#setProperty<T, K extends keyof T>(target: T, key: K, value: T[K]) {
+		Object.defineProperty(target, key, {
+			configurable: true,
+			writable: false,
+			value,
+		});
+
+		if (target[key] !== value) {
+			throw new Error(`Unable to set property '${key}'.`);
+		}
+	}
+
+	#handleBefore<TFunction extends AnyFunction>(
+		that: ThisParameterType<TFunction>,
+		args: Parameters<TFunction>,
+		handler: BeforeCallback<TFunction>
+	) {
+		let returnValue: ReturnType<TFunction>;
+		let returnSet = false;
+		let currentArgs = args;
+		let currentThis = that;
+		const handled = handler({
+			get arguments() {
+				return currentArgs;
+			},
+			set arguments(value) {
+				currentArgs = value;
+			},
+			get this() {
+				return currentThis;
+			},
+			set this(value) {
+				currentThis = value;
+			},
+			next() {
+				return snoopycall;
+			},
+			return(value) {
+				returnSet = true;
+				returnValue = value;
+				return snoopycall;
+			},
+		});
+		const getResp = () => ({ currentArgs, currentThis, ...(returnSet ? { returnValue: returnValue! } : {}) });
+		if (handled instanceof Promise) {
+			return handled.then((p) => getResp());
+		}
+		return getResp();
+	}
+
+	#handleAfter<TFunction extends AnyFunction>(
+		currentThis: ThisParameterType<TFunction>,
+		currentArgs: Parameters<TFunction>,
+		origThis: ThisParameterType<TFunction>,
+		origArgs: Parameters<TFunction>,
+		handler: AfterCallback<TFunction>,
+		returnValue: ReturnType<TFunction>
+	) {
+		const handled = handler({
+			arguments: currentArgs,
+			this: currentThis,
+			get returnValue() {
+				return returnValue;
+			},
+			set returnValue(value) {
+				returnValue = value;
+			},
+			original: {
+				arguments: origArgs,
+				this: origThis,
+			},
+			next() {
+				return snoopycall;
+			},
+			return(value: ReturnType<TFunction>) {
+				returnValue = value;
+				return snoopycall;
+			},
+		});
+		if (handled instanceof Promise) {
+			return handled.then(() => returnValue);
+		}
+		return returnValue;
+	}
+
 	// patch a function, before/after
-	public function<T, K extends KeysOfType<T, AnyFunction>>(target: T, fnKey: K, callbacks: PatchConfig<T[K]>): void {}
+	public function<T, K extends KeysOfType<T, AnyFunction>>(target: T, fnKey: K, callbacks: PatchConfig<T[K]>): void {
+		const self = this;
+		const original: AnyFunction = target[fnKey];
+		if (typeof original !== "function") {
+			throw new Error(`Property '${fnKey}' on target must be of type 'function'. Actual type: '${typeof original}'.`);
+		}
+
+		const patch = function (this: unknown) {
+			type fn = T[K];
+
+			const origArgs = [...arguments] as Parameters<fn>;
+			const origThis = this as ThisParameterType<fn>;
+			let currentArgs = [...arguments] as Parameters<fn>;
+			let currentThis = this as ThisParameterType<fn>;
+
+			let after = () => {
+				let returnValue: ReturnType<fn> = original.apply(currentThis, currentArgs);
+
+				if (callbacks.after) {
+					const h = self.#handleAfter(currentThis, currentArgs, origThis, origArgs, callbacks.after, returnValue);
+					if (h instanceof Promise) {
+						return h;
+					} else {
+						returnValue = h;
+					}
+				}
+				return returnValue;
+			};
+
+			let next = (h: {
+				returnValue?: NonNullable<ReturnType<T[K]>> | undefined;
+				currentArgs: Parameters<T[K]>;
+				currentThis: ThisParameterType<T[K]>;
+			}) => {
+				if ("returnValue" in h) {
+					return h.returnValue!;
+				}
+				({ currentThis, currentArgs } = h);
+				return after();
+			};
+
+			if (callbacks.before) {
+				const handle = self.#handleBefore(currentThis, currentArgs, callbacks.before);
+				if (handle instanceof Promise) {
+					return handle.then(next);
+				} else {
+					return next(handle);
+				}
+			}
+			return after();
+		};
+
+		this.#setProperty(target, fnKey, patch as T[K]);
+	}
+
+	// public event<T extends EventEmittingObject>(
+	// 	target: EventEmittingObject,
+	// 	name: Parameters<EventEmittingObject["addEventListener"]>[0]
+	// ) {}
 
 	// patch a property, get/set before/after
 	// public prop<T, K extends keyof T>(
@@ -44,4 +187,31 @@ class Snoopy {
 	// 	}
 	// ): void {}
 }
-export default new Snoopy();
+// export default new Snoopy();
+
+// new Snoopy().event(window, "xxxxx");
+
+// declare const c: {
+// 	addEventListener<K extends keyof WindowEventMap>(
+// 		type: K,
+// 		listener: (this: Window, ev: WindowEventMap[K]) => any,
+// 		options?: boolean | AddEventListenerOptions
+// 	): void;
+// 	addEventListener(
+// 		type: string,
+// 		listener: EventListenerOrEventListenerObject,
+// 		options?: boolean | AddEventListenerOptions
+// 	): void;
+// };
+
+// type x = Parameters<typeof c["addEventListener"]>;
+
+// type p = { [k in keyof typeof c]: typeof c[k] extends Function ? 1 : 0 };
+
+// type EventEmittingObject = {
+// 	addEventListener<K extends string>(
+// 		type: K,
+// 		listener: EventListenerOrEventListenerObject,
+// 		options?: boolean | AddEventListenerOptions
+// 	): void;
+// };
--- tests/function.spec.ts
+++ tests/function.spec.ts
@@ -1,23 +1,25 @@
 import { expect } from "chai";
 import snoopy from "../src";
 describe("snoopy", () => {
+	const other = { world: "hello" };
+
+	type TargetReturn<TRet, TArgs> = { r: TRet; a: TArgs; t: unknown };
+	type Target<TRet, TArgs extends any[]> = {
+		fn(...args: TArgs): TargetReturn<TRet, TArgs>;
+	};
+	function target<TArgs extends any[] = []>(): Target<void, TArgs>;
+	function target<TRet, TArgs extends any[] = [string]>(ret: TRet): Target<TRet, TArgs>;
+	function target<TRet, TArgs extends any[] = [string]>(ret?: TRet) {
+		return (function () {
+			return {
+				fn: function (...args: TArgs) {
+					return { r: ret, a: args, t: this };
+				},
+			};
+		})();
+	}
+
 	describe("function:before", () => {
-		const that = { hello: "world" };
-		type TargetReturn<TRet, TArgs> = { r: TRet; a: TArgs; t: unknown };
-		type Target<TRet, TArgs extends any[]> = {
-			fn(...args: TArgs): TargetReturn<TRet, TArgs>;
-		};
-		function target<TArgs extends any[] = []>(): Target<void, TArgs>;
-		function target<TRet, TArgs extends any[] = [string]>(ret: TRet): Target<TRet, TArgs>;
-		function target<TRet, TArgs extends any[] = [string]>(ret?: TRet) {
-			return (function () {
-				return {
-					fn: function (...args: TArgs) {
-						return { r: ret, a: args, t: this };
-					},
-				};
-			})();
-		}
 		it("can pass on to original function", function (this: any) {
 			const t = target<[string]>();
 			t.fn.call("xx");
@@ -35,7 +37,7 @@ describe("snoopy", () => {
 		});
 		it("can return before calling original function", () => {
 			const t = target(123);
-			expect(t.fn("")).to.eq(123);
+			expect(t.fn("").r).to.eq(123);
 			snoopy.function(t, "fn", {
 				before(ctx) {
 					return ctx.return({
@@ -45,7 +47,7 @@ describe("snoopy", () => {
 					});
 				},
 			});
-			expect(t.fn("")).to.eq(999);
+			expect(t.fn("").r).to.eq(999);
 		});
 		it("receives arguments", () => {
 			const t = target<number, [number, number, number]>(1);
@@ -71,7 +73,7 @@ describe("snoopy", () => {
 				},
 			});
 			t.fn();
-			expect(self).to.eq(that);
+			expect(self).to.eq(t);
 		});
 		it("can replace individual arguments", () => {
 			const t = target<[string, number, boolean]>();
@@ -92,18 +94,17 @@ describe("snoopy", () => {
 					return ctx.next();
 				},
 			});
-			expect(t.fn("aaa", 1, true).a).to.deep.eq([{ args: ["zzz", 999, false] }]);
+			expect(t.fn("aaa", 1, true).a).to.deep.eq(["zzz", 999, false]);
 		});
 		it("can replace 'this' context", () => {
 			const t = target<[string, number, boolean]>();
-			const mine = { world: "hello" };
 			snoopy.function(t, "fn", {
 				before(ctx) {
-					ctx.this = mine;
+					ctx.this = other;
 					return ctx.next();
 				},
 			});
-			expect(t.fn("aaa", 1, true).t).to.eq(mine);
+			expect(t.fn("aaa", 1, true).t).to.eq(other);
 		});
 		it("can throw exception", () => {
 			const t = target();
@@ -115,7 +116,11 @@ describe("snoopy", () => {
 			try {
 				t.fn();
 				expect.fail("did not error");
-			} catch (error) {}
+			} catch (error) {
+				if (!error.toString().includes("catch me")) {
+					throw error;
+				}
+			}
 		});
 		it("can run async code when original return is a promise", async () => {
 			let retVal = 123;
@@ -140,11 +145,106 @@ describe("snoopy", () => {
 	});
 
 	describe("function:after", () => {
-		it("receives used arguments", () => {});
-		it("receives used 'this' context", () => {});
-		it("receives original arguments", () => {});
-		it("receives original 'this' context", () => {});
-		it("receives return value", () => {});
-		it("can run async code when original return is a promise", () => {});
+		it("receives used & original arguments", () => {
+			type args = [string, number, boolean];
+			const t = target<args>();
+			let origArgs: args | undefined = undefined;
+			let args: args | undefined = undefined;
+			snoopy.function(t, "fn", {
+				before(ctx) {
+					ctx.arguments = ["zzz", 999, false];
+					return ctx.next();
+				},
+				after(ctx) {
+					origArgs = ctx.original.arguments;
+					args = ctx.arguments;
+					return ctx.next();
+				},
+			});
+			t.fn("aaa", 111, true);
+			expect(origArgs).to.deep.eq(["aaa", 111, true]);
+			expect(args).to.deep.eq(["zzz", 999, false]);
+		});
+		it("receives used & original 'this' context", () => {
+			const t = target<[string, number, boolean]>();
+			let origThis: unknown | undefined = undefined;
+			let theThis: unknown | undefined = undefined;
+			snoopy.function(t, "fn", {
+				before(ctx) {
+					ctx.this = other;
+					return ctx.next();
+				},
+				after(ctx) {
+					origThis = ctx.original.this;
+					theThis = ctx.this;
+					return ctx.next();
+				},
+			});
+			expect(t.fn("aaa", 111, true).t).to.eq(other);
+			expect(origThis).to.eq(t);
+			expect(theThis).to.eq(other);
+		});
+		it("receives & can change return value", () => {
+			const t = target("a");
+			let origRet: TargetReturn<string, [string]> | undefined = undefined;
+			snoopy.function(t, "fn", {
+				after(ctx) {
+					origRet = ctx.returnValue;
+					return ctx.return({
+						a: ["xxx"],
+						r: "x",
+						t: other,
+					});
+				},
+			});
+
+			expect(t.fn("aaa")).to.deep.eq({
+				a: ["xxx"],
+				r: "x",
+				t: other,
+			} as TargetReturn<string, [string]>);
+
+			expect(origRet).to.deep.eq({
+				a: ["aaa"],
+				r: "a",
+				t,
+			} as TargetReturn<string, [string]>);
+		});
+		it("can run async code when original return is a promise", async () => {
+			let retVal = 123;
+			const t = {
+				async fn(...args: any) {
+					return { r: retVal, a: args, t: this };
+				},
+			};
+
+			snoopy.function(t, "fn", {
+				after(ctx) {
+					return new Promise(async (r) => {
+						await new Promise((v) => setTimeout(v, 100));
+						(await ctx.returnValue).r = 999;
+						r(ctx.next());
+					});
+				},
+			});
+
+			expect((await t.fn()).r).to.eq(999);
+		});
+		it("can throw exception", () => {
+			const t = target();
+			snoopy.function(t, "fn", {
+				after() {
+					throw new Error("catch me");
+				},
+			});
+			try {
+				t.fn();
+				expect.fail("did not error");
+			} catch (error) {
+				if (!error.toString().includes("catch me")) {
+					throw error;
+				}
+			}
+		});
 	});
 });
